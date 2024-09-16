3f45722828423cd6d47ec67d0bf7d51dd98cc8ff
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-09-16 13:06:16 -0600

    Added morgan package for logging requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -11,13 +12,15 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetsRouter);
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
