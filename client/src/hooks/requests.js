3559e253c6b3e69b7afa308c6c34425050cc1f80
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-09-17 22:24:50 -0600

    Creating connection to POST new launches from the UI

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -20,6 +20,17 @@ async function httpGetLaunches() {
 async function httpSubmitLaunch(launch) {
   // TODO: Once API is ready.
   // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (error) {
+    return { ok: false };
+  }
 }
 
 async function httpAbortLaunch(id) {
