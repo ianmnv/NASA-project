d46bdb0961db23d416b9b3614e309c2adf862253
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-09-17 18:49:23 -0600

    Creating validation for POST requests

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,9 +1,36 @@
-const { getAllLaunches } = require("../../models/launches.model");
+const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
 }
 
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: "Missing properties to launch a mission.",
+    });
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid date, try for example 'January 1, 2030'.",
+    });
+  }
+
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
+}
+
 module.exports = {
   httpGetAllLaunches,
+  httpAddNewLaunch,
 };
