6d6e953b9562589f1c8aac2e2675806a9e81c1d4
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-09-18 23:05:09 -0600

    Installing Jest and trying it out

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,12 @@
+describe("Test GET /launches", () => {
+  test("Should return 200 status code", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /launch", () => {
+  test("Should return 200 status code", () => {});
+  test("Should catch for missing require properties", () => {});
+  test("Should catch invalid dates", () => {});
+});
