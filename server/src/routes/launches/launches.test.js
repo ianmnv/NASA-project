0358239f015928833e26d2980f56b08f9b2d3d00
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-09-19 14:23:42 -0600

    Testing POST requests in case of success

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -13,7 +13,33 @@ describe("Test GET /launches", () => {
 });
 
 describe("Test POST /launch", () => {
-  test("Should return 200 status code", () => {});
+  const completeLaunchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1791",
+    target: "Kepler 1293-a",
+    launchDate: "January 1, 2025",
+  };
+
+  const launchDataWithoutDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1791",
+    target: "Kepler 1293-a",
+  };
+
+  test("Should return 201 created status code", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-type", /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(requestDate).toBe(responseDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
+
   test("Should catch for missing require properties", () => {});
   test("Should catch invalid dates", () => {});
 });
