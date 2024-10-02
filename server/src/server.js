1e5b3cd9d9fc3d6511bfcddd5564574f3c5d4d59
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-10-01 20:45:12 -0600

    Connecting API to MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
@@ -6,9 +7,21 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api-ian:DeJSQQhsN2wGcMs6@nasacluster.omu3z.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.error(error);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
